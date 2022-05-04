8804c5fd683c4373fb79dea44e00c9ae7b44287e
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-05-04 10:05:34 +0200

    START 7 - Bootstrap theme & Type customization

--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Book;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,13 +15,16 @@ class BookType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('isbn')
+            ->add('title', TextType::class)
+            ->add('isbn', TextType::class, [
+                'label' => 'ISBN',
+            ])
             ->add('releasedAt', DateType::class, [
                 'widget' => 'single_text',
                 'input' => 'datetime_immutable',
+                'label' => 'Publication date',
             ])
-            ->add('plot')
+            ->add('plot', TextareaType::class)
         ;
     }
 
