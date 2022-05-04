a763521780cbecb0db745bddff507056a79133c6
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-05-04 10:04:05 +0200

    START 7 - Create BookType

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Form\BookType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/book', name: 'app_book_')]
 class BookController extends AbstractController
 {
-    #[Route('/book/{page<\d+>?1}', name: 'app_book')]
+    #[Route('/{page<\d+>?1}', name: 'index')]
     public function index(int $page): Response
     {
         return $this->render('book/index.html.twig', [
             'controller_name' => 'BookController',
+        ]);
+    }
+
+    #[Route('/create', name: 'create')]
+    public function create(): Response
+    {
+        $form = $this->createForm(BookType::class);
+
+        return $this->renderForm('book/create.html.twig', [
+            'book_form' => $form,
         ]);
     }
 }
