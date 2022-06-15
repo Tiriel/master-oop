bb725ac40cee00c03da000c08c944ab7b19a619f
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-06-15 13:15:23 +0200

    Notification factories

--- src/Notifier/MovieOrderNotifier.php
+++ src/Notifier/MovieOrderNotifier.php
@@ -7,17 +7,18 @@ use Symfony\Component\Notifier\NotifierInterface;
 
 class MovieOrderNotifier
 {
+    private iterable $factories;
     private NotifierInterface $notifier;
 
-    public function __construct(NotifierInterface $notifier)
+    public function __construct(iterable $factories, NotifierInterface $notifier)
     {
+        $this->factories = $factories instanceof \Traversable ? iterator_to_array($factories) : $factories;
         $this->notifier = $notifier;
     }
 
     public function sendNotification(string $channel, string $message): bool
     {
-        // TODO: Rework to instantiate proper notification according to channel
-        $notification = new Notification();
+        $notification = $this->factories[$channel]->getNotification();
         $this->notifier->send($notification);
 
         return true;
